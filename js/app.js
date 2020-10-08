7971d83ca4352582006b82cb071a9222dff1605b
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-10-08 08:53:17 -0400

    Create app.js
    Inputed inline js to app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+    window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
+
+
