a0c077ed8c49b81f221ec10d11e1dbd9c600c13b
Webdevak <66316345+webdevak@users.noreply.github.com>
2020-11-10 02:10:58 -0500

    Added js pluggins

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@
 }, false);
 
 
+
+
+let tl = new TimelineMax({onUpdate:updatePercentage});
+const controller = new ScrollMagic.Controller();
+
+// tl.from("#projects", .5, {x:200, opacity:0});
+
+const scene = new ScrollMagic.scene({
+  triggerElement: ".projects",
+  triggerHook: "onLeave",
+  duration:"100%"
+})
+
+.setPin(".projects")
+.setTween(tl)
+.addTo(controller);
+
+function updatePercentage() {
+  tl.progress();
+  console.log(tl.progress());
+}
